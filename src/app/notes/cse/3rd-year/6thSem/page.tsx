f95eae80aa4e39197c1e52800acde2f1678a3d8d
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:37:41 +0530

    feat: Add 6th Sem page component for displaying 6th semester notes

---
+++ src/app/notes/cse/3rd-year/6thSem/page.tsx
@@ -0,0 +1,8 @@
+export default function SixthSem() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>     6th sem  page Overview </h1>
+      </div>
+    );
+  }
+  
