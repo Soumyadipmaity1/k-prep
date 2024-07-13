e715b4c34dc873eec3bb11b897b49dc7ae6cc4d4
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:37:17 +0530

    feat: Add 4th Sem page component for displaying 4th semester notes

---
+++ src/app/notes/cse/2nd-year/4thSem/page.tsx
@@ -0,0 +1,8 @@
+export default function ForthSem() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>     4th sem  page Overview </h1>
+      </div>
+    );
+  }
+  
