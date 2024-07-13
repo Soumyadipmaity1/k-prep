7786a4837102ee2343df6c9063828b064529c718
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:36:50 +0530

    feat: Add SchemeA page component for Scheme A notes

---
+++ src/app/notes/cse/1st-year/schemeA/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SchemeA() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>   Scheme A page Overview </h1>
+      </div>
+    );
+  }
+  
