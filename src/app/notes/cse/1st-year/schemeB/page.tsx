f72a0615a8678f5c241a52bcaf176bc46c2afb66
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:36:57 +0530

    feat: Add SchemeB page component for Scheme B notes

---
+++ src/app/notes/cse/1st-year/schemeB/page.tsx
@@ -0,0 +1,8 @@
+export default function SchemeB() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>    Scheme B page Overview </h1>
+      </div>
+    );
+  }
+  
