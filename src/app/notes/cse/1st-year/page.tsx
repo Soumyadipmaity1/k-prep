a5f7d83019576989252a3a6fa1d9a4ed57567913
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:36:42 +0530

    feat: Add CSE page component for displaying Computer Science and Engineering notes

---
+++ src/app/notes/cse/1st-year/page.tsx
@@ -0,0 +1,9 @@
+
+export default function FirstYear() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>  1st year page Overview </h1>
+      </div>
+    );
+  }
+  
