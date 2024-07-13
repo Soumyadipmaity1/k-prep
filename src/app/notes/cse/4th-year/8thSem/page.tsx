bb9e77281162169a45928a7ae46afd7a04859126
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:38:00 +0530

    feat: Add 8th Sem page component for displaying 8th semester notes

---
+++ src/app/notes/cse/4th-year/8thSem/page.tsx
@@ -0,0 +1,8 @@
+export default function EighthSem() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>     8th sem  page Overview </h1>
+      </div>
+    );
+  }
+  
