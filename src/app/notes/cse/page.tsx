b7338536bd2f407d92e8b47f5d1f6bdfeb23b114
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:36:33 +0530

    feat: Create CSE page component for displaying Computer Science and Engineering notes

---
+++ src/app/notes/cse/page.tsx
@@ -0,0 +1,9 @@
+
+export default function CSE() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1> CSE page Overview </h1>
+      </div>
+    );
+  }
+  
