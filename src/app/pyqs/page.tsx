0c38fbd3b2ab93adef1a3b24d9159f49f0060f1e
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:38:11 +0530

    feat: Add Pyqs page component for displaying pyqs

---
+++ src/app/pyqs/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Pyqs() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>Pyqs page here </h1>
+      </div>
+    );
+  }
+  
