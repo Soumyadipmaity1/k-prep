111482714fbfc0a24c2b8d257ff0f54b1511ed93
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:37:53 +0530

    feat: Add 7th Sem page component for displaying 7th semester notes

---
+++ src/app/notes/cse/4th-year/7thSem/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SeventhSem() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>   7th Sem page Overview </h1>
+      </div>
+    );
+  }
+  
