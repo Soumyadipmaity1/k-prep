804d032610a9b5fca7992874677dc7e3edd262e8
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:37:03 +0530

    feat: Add 2nd year page component for displaying 2nd year notes

---
+++ src/app/notes/cse/2nd-year/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SecondYear() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>  2nd year page Overview </h1>
+      </div>
+    );
+  }
+  
