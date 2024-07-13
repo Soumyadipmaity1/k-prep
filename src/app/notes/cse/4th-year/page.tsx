5be9ea80af47b6510f702ae71d4bf2d4949f4289
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-13 18:37:47 +0530

    feat: Add 4th Year page component for displaying 4th year notes

---
+++ src/app/notes/cse/4th-year/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ForthYear() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>  4th year page Overview </h1>
+      </div>
+    );
+  }
+  
