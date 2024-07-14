c973a366596e75064c2e2e50d6b1663dbd823b00
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-14 23:48:06 +0530

    refactor: Add Labs page component

---
+++ src/app/labs/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Labs() {
+    return (
+      <div className="h-screen flex justify-center items-center text-5xl">
+        <h1>Labs page </h1>
+      </div>
+    );
+  }
+  
