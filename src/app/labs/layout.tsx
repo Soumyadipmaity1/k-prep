fdaa720767a7e28ce7c69fa7b46e30c6e69baada
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-14 23:47:56 +0530

    refactor: Update LabsLayout component to include Navbar and Sidebar components

---
+++ src/app/labs/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Menubar/Navbar/Navbar2";
+import Sidebar from "@/components/Menubar/SideBar/Sidebar"; 
+
+const inter = Inter({ subsets: ["latin"] });
+
+const LabsLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={inter.className}>
+      <Navbar />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      <div className={`container mx-auto p-4 transition-all duration-300 ${isOpen ? "ml-64" : ""}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default LabsLayout;
