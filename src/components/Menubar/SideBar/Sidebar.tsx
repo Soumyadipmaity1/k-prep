6d17c2be76ba80426adcad18ee14abb704253c5d
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-14 23:12:32 +0530

    refactor: Update Sidebar component to use new icons and improve styling

--- src/components/Menubar/SideBar/Sidebar.tsx
+++ src/components/Menubar/SideBar/Sidebar.tsx
@@ -1,63 +1,28 @@
-"use client";
+// src/components/Menubar/SideBar/Sidebar.tsx
+import { AiFillHome,  } from 'react-icons/ai';
+import { FaUserGraduate, FaChalkboardTeacher, FaBookOpen, FaLaptopCode, FaCalendarAlt } from 'react-icons/fa';
 
-import React from "react";
-import Link from "next/link";
-import { FaHome, FaBook, FaFlask, FaBars, FaTimes } from "react-icons/fa";
-
-type SidebarProps = {
-  isOpen: boolean;
-  toggleSidebar: () => void;
-};
-
-const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
+const Sidebar = ({ isOpen, toggleSidebar }: { isOpen: boolean, toggleSidebar: () => void }) => {
   return (
-    <>
-      <div className="fixed top-0 left-0 p-4 z-50">
-        <button onClick={toggleSidebar}>
-          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-        </button>
+    <div
+      className={`fixed top-14 left-0 h-full transition-all duration-500 ${isOpen ? 'w-48' : 'w-16'} bg-purple-900 text-white`}
+    >
+      <div className="flex flex-col items-center py-4">
+        <SidebarIcon icon={<AiFillHome size="24" />} name="Home" isOpen={isOpen} toggleSidebar={toggleSidebar} />
+        <SidebarIcon icon={<FaChalkboardTeacher size="24" />} name="1st Year " isOpen={isOpen} toggleSidebar={toggleSidebar} />
+        <SidebarIcon icon={<FaBookOpen size="24" />} name="2nd year" isOpen={isOpen} toggleSidebar={toggleSidebar} />
+        <SidebarIcon icon={<FaLaptopCode size="24" />} name="3rd Year" isOpen={isOpen} toggleSidebar={toggleSidebar} />
+        <SidebarIcon icon={<FaUserGraduate size="24" />} name="4th Year" isOpen={isOpen} toggleSidebar={toggleSidebar} />
       </div>
-      <div
-        className={`fixed top-0 left-0 h-full bg-white shadow-md w-64 transform transition-transform duration-300 z-40 ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        <div className="p-4 mt-12">
-          <h2 className="text-2xl font-bold mb-4">Menu</h2>
-          <ul className="space-y-4">
-            <li>
-              <Link href="/notes/cse">
-                <div className="flex items-center cursor-pointer">
-                  <FaHome className="mr-2" /> 1st year
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/notes/2nd-year">
-                <div className="flex items-center cursor-pointer">
-                  <FaBook className="mr-2" /> 2nd year
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/notes/3rd-year">
-                <div className="flex items-center cursor-pointer">
-                  <FaFlask className="mr-2" /> 3rd year
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/notes/4th-year">
-                <div className="flex items-center cursor-pointer">
-                  <FaFlask className="mr-2" /> 4th year
-                </div>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
+
+const SidebarIcon = ({ icon, name, isOpen, toggleSidebar }: { icon: React.ReactNode, name: string, isOpen: boolean, toggleSidebar: () => void }) => (
+  <div className="flex items-center my-4 w-full cursor-pointer" onClick={toggleSidebar}>
+    <div className="flex-shrink-0 ml-4">{icon}</div>
+    {isOpen && <span className="ml-4">{name}</span>}
+  </div>
+);
 
 export default Sidebar;
