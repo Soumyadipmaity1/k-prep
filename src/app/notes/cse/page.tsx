caa676ac1e43f50e8b82ad714fe529cc397f91d3
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-15 23:46:05 +0530

    refactor: Remove empty h1 tag from CSE page

--- src/app/notes/cse/page.tsx
+++ src/app/notes/cse/page.tsx
@@ -2,7 +2,6 @@
 export default function CSE() {
     return (
       <div className="h-screen flex justify-center items-center text-5xl">
-        
         <h1> CSE page Overview </h1>
       </div>
     );
