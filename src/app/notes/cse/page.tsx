a11ef836d747c8b8da7645491e8933e46903ef1e
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-15 23:45:46 +0530

    refactor: Add CSE page component

--- src/app/notes/cse/page.tsx
+++ src/app/notes/cse/page.tsx
@@ -2,6 +2,7 @@
 export default function CSE() {
     return (
       <div className="h-screen flex justify-center items-center text-5xl">
+        
         <h1> CSE page Overview </h1>
       </div>
     );
