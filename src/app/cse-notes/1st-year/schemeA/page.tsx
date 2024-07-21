d9bd92b697b51167a3abbb969ee210b04c67ea1d
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-21 16:44:13 +0530

    refactor: Add SchemeA component for Scheme A notes page

---
+++ src/app/cse-notes/1st-year/schemeA/page.tsx
@@ -0,0 +1,13 @@
+
+const SchemeA = () => {
+    return (
+        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+          <div className="max-h-screen font-bold p-10">
+<p>Scheme A</p>
+          </div>
+      </div>
+    );
+  };
+  
+  export default SchemeA;
+  
