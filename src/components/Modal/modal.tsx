93004a28658bc3f228eafca1c06d6611d83579d2
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-21 12:53:34 +0530

    refactor: Update ModalMenu component with Dropdown for branch selection

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -1,7 +1,8 @@
-"use client"; 
+"use client";
 
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from '../dropdown/Dropdown';
 
 interface ModalMenuProps {
   isOpen: boolean;
@@ -11,6 +12,7 @@ interface ModalMenuProps {
 
 const ModalMenu: React.FC<ModalMenuProps> = ({ isOpen, onClose, items }) => {
   const router = useRouter();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   if (!isOpen) return null;
 
@@ -19,25 +21,23 @@ const ModalMenu: React.FC<ModalMenuProps> = ({ isOpen, onClose, items }) => {
     onClose();
   };
 
+  const handleDropdownToggle = (open: boolean) => {
+    setIsDropdownOpen(open);
+  };
+
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-      <div className="bg-[#f8e9f4] p-6 rounded-lg shadow-lg  w-[800px] relative text-center">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 text-2xl">
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
+      <div
+        className={`bg-[#f8e9f4] p-8 rounded-lg shadow-lg relative text-center transition-all duration-300 ease-in-out ${
+          isDropdownOpen ? 'h-[450px]' : 'h-60'
+        } w-[800px]`}
+      >
+        <button onClick={onClose} className="absolute top-2 right-4 text-gray-600 text-4xl">
           &times;
         </button>
-        <h2 className="text-4xl font-semibold mb-4 modal-text">Hello, Loveable KIITkats! </h2>
-        <p className="mb-4 text-3xl modal-text">Please select your branch.</p>
-        <div className="space-y-2">
-          {items.map((item) => (
-            <button
-              key={item.route}
-              onClick={() => handleItemClick(item.route)}
-              className="w-full text-left px-4 py-2 bg-gradient-to-r from-pink-400 to-purple-500 text-white rounded hover:from-pink-500 hover:to-purple-600"
-            >
-              {item.label}
-            </button>
-          ))}
-        </div>
+        <h2 className="text-4xl font-semibold mb-4 modal-text">Hello, Loveable KIITkats!</h2>
+        <p className="mb-4 text-2xl modal-text">Please select your branch.</p>
+        <Dropdown items={items} onItemClick={handleItemClick} onDropdownToggle={handleDropdownToggle} />
       </div>
     </div>
   );
