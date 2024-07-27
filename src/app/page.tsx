5e97a4d5f4d8be09a4c8280ca13ef3904ccac79c
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-27 12:18:57 +0530

    refactor: Enable lazy loading for SliderComponent in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import FeatureSection from "@/components/landingpage/Feature/FeatureSection";
 import Team from "@/components/landingpage/Team/team";
 import Testimonials from "@/components/landingpage/Testimonial/testinomial";
 import { denkOne } from './font';
+import dynamic from 'next/dynamic';
+const MaterialSlider = dynamic(() => import('../components/Slider/SliderComponent'), { ssr: false });
 
 export default function Home() {
   return (
     <div className={denkOne.className}>
+      {/* <MaterialSlider/> */}
       <FeatureSection />
       <Testimonials />
       <Team />
