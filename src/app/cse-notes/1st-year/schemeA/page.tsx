c09c12502e53239580f2421a1a2bc6ba2bb82a11
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-27 12:19:37 +0530

    feat: Update SchemeA page with subject selection button

--- src/app/cse-notes/1st-year/schemeA/page.tsx
+++ src/app/cse-notes/1st-year/schemeA/page.tsx
@@ -1,9 +1,15 @@
+import SubjectButton from "@/components/Subjects/SchemeASub";
 
 const SchemeA = () => {
     return (
         <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
           <div className="max-h-screen font-bold p-10">
-<p>Scheme A</p>
+<h1 className="modal-text text-center text-4xl">Pick a subject for your notes!</h1>
+<div className="mt-10">
+
+  <SubjectButton/>
+</div>
+
           </div>
       </div>
     );
