3180b0eaeb978a6e419924e0113eda266049151f
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-02 11:18:46 +0530

    feat: Add Hero component to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Team from "@/components/landingpage/Team/team";
 import Testimonials from "@/components/landingpage/Testimonial/testinomial";
 import { denkOne } from './font';
 import dynamic from 'next/dynamic';
+import Hero from '@/components/landingpage/Hero/Hero';
 const MaterialSlider = dynamic(() => import('../components/Slider/SliderComponent'), { ssr: false });
 
 export default function Home() {
   return (
     <div className={denkOne.className}>
+      <Hero/>
       {/* <MaterialSlider/> */}
       <FeatureSection />
       <Testimonials />
