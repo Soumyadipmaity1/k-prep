11d45a92c6643fc770f0228f0e321aae9c77e8df
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-03 09:24:47 +0530

    feat: Add page component for 2nd year 3rd semester notes

---
+++ src/app/cse-notes/2nd-year/3rdSem/page.tsx
@@ -0,0 +1,19 @@
+import SubjectButton from "@/components/Subjects/2nd-year/3rdSenSubject";
+
+const SchemeA = () => {
+    return (
+        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+          <div className="max-h-screen font-bold p-10">
+<h1 className=" text-center text-4xl"><span className="modal-text">Pick a subject for your notes!</span></h1>
+<div className="mt-10">
+
+  <SubjectButton/>
+</div>
+
+          </div>
+      </div>
+    );
+  };
+  
+  export default SchemeA;
+  
