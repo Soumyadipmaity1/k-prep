cdf2b811dd23743085e771dc26c5dc87b1d229f3
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-03 09:24:38 +0530

    feat: Add SecondYear page component for 2nd year notes

---
+++ src/app/cse-notes/2nd-year/page.tsx
@@ -0,0 +1,13 @@
+
+const SecondYear = () => {
+    return (
+        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+          <div className="max-h-screen font-bold p-10">
+2nd year
+          </div>
+      </div>
+    );
+  };
+  
+  export default SecondYear;
+  
