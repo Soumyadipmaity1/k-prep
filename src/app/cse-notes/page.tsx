0345baa8de1a422c808d7c467797641b8ae77374
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-03 09:21:33 +0530

    refactor: Add left margin to CseNotesPage for better alignment

--- src/app/cse-notes/page.tsx
+++ src/app/cse-notes/page.tsx
@@ -2,8 +2,8 @@ import CardResources from "@/components/RecommendedNotes/1st-Year/NotesCard";
 
 const CseNotesPage = () => {
   return (
-      <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto overflow-x-hidden border rounded-xl border-gray-300">
-        <div className="bg-[#f8e9f4]  h-screen p-10">
+      <div className="w-full h-full ml-2 bg-[#f8e9f4] overflow-y-auto overflow-x-hidden border rounded-xl border-gray-300">
+        <div className="bg-[#f8e9f4] mx-auto h-screen p-10">
   <h1 className="text-4xl font-bold mb-5">Views Recommended Notes for CSE</h1>
     <div className="bg-[#f8e9f4] ">
       <h1 className="mb-4 text-2xl pl-1">1st Year Notes:</h1>
