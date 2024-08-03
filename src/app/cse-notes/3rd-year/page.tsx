129874a37e92c3d13987a9990aa32bac602e70d9
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-03 09:26:50 +0530

    feat: Add page component for 3rd year notes

---
+++ src/app/cse-notes/3rd-year/page.tsx
@@ -0,0 +1,13 @@
+
+const FirstYear = () => {
+    return (
+        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+          <div className="max-h-screen font-bold p-10">
+1st year
+          </div>
+      </div>
+    );
+  };
+  
+  export default FirstYear;
+  
