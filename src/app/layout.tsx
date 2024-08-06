3b02ff1e399a45b8bbcee61011eefb431abcc87f
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-07 03:12:41 +0530

    refactor: Update body styling in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={denkOne.className}>
-      <body className="bg-white p-5 body-shadow m-5 rounded-xl">
-        <Navbar />
+      <body className=" sm:p-5 body-shadow sm:m-5 m-3 p-2 rounded-md sm:rounded-xl">
+<div className='hidden sm:block'>
+<Navbar />
+
+</div>
         {children}
         <Footer />
       </body>
