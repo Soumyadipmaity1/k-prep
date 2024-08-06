4e5090b9398e9b129d2571f28199ff50770681e3
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-07 03:12:57 +0530

    refactor: Update border color in 6thSem page

--- src/app/cse-notes/3rd-year/6thSem/page.tsx
+++ src/app/cse-notes/3rd-year/6thSem/page.tsx
@@ -2,7 +2,7 @@ import SubjectButton from "@/components/Subjects/2nd-year/4thSemSubject";
 
 const SchemeA = () => {
     return (
-        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-900">
           <div className="max-h-screen font-bold p-10">
 <h1 className=" text-center text-4xl"><span className="modal-text">Pick a subject for your notes!</span></h1>
 <div className="mt-10">
