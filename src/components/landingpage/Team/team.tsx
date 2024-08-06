424a4f7e5b482f509ebb7c495272b17986676171
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-07 03:13:29 +0530

    refactor: Update landing page Team section styling and responsiveness

--- src/components/landingpage/Team/team.tsx
+++ src/components/landingpage/Team/team.tsx
@@ -4,16 +4,16 @@ import { teamMembers } from './memberDetails';
 
 const Team: React.FC = () => {
   return (
-    <div className=' bg-gradient-to-r rounded-xl bg-team from-[#C476F0] to-[#EE85BB] p-10'>
+    <div className=' bg-gradient-to-r rounded-xl bg-team from-[#C476F0] to-[#EE85BB] py-10 sm:p-10'>
       <div className="text-center">
         <h1 className="text-5xl font-bold tracking-[0.5px] text-[#231D1F]">Our Team</h1>
-        <p className="p-5 pb-10  px-20">
+        <p className="p-5 pb-10  sm:px-20">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magni cupiditate eaque soluta excepturi
           corporis vel voluptate modi tempora perferendis. Consequatur libero a sapiente ullam, repudiandae eum et
           similique nulla!
         </p>
       </div>
-      <div className="flex flex-wrap justify-around pt-10 mx-20">
+      <div className="flex flex-wrap justify-around pt-10 sm:mx-20">
         {teamMembers.map((member, index) => (
           <div key={index} className="flex flex-col items-center justify-center mb-10">
             <div className="mb-4">
@@ -33,7 +33,7 @@ const Team: React.FC = () => {
         ))}
       </div>
       <div className='mx-auto justify-center flex mt-5'>
-<button type="button" className='bg-[#FBD6FF] shadow-lg hover:shadow-[0px_8px_4px_rgba(0,0,0,0.25)] rounded-[60px] p-3 px-10 text-2xl  text-center'>WANT TO BECOME A MEMBER?</button>
+<button type="button" className='bg-[#FBD6FF] shadow-lg hover:shadow-[0px_8px_4px_rgba(0,0,0,0.25)] rounded-[60px] sm:p-3 p-2 sm:px-10 px-8 sm:text-2xl text-[22px]  text-center'>WANT TO BECOME A MEMBER?</button>
 
       </div>
     </div>
