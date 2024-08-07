942bdaa977dc4290c441e772348527abe7fdb27c
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-08 01:45:01 +0530

    feat: Create new page component for CSE Pyqs

---
+++ src/app/cse-pyqs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='sm:text-5xl text-4xl flex justify-center items-center h-screen font-extrabold'>
+      Will Be Updated Soon !
+    </div>
+  )
+}
+
+export default page
