1d8d515803ea78d8d483175ddc947b3ec30a3493
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-07 13:39:52 +0530

    refactor: Update landing page FeatureCard component styling for responsiveness

--- src/components/landingpage/Feature/FeatureCard.tsx
+++ src/components/landingpage/Feature/FeatureCard.tsx
@@ -20,8 +20,8 @@ const FeatureCard: React.FC<ContactOptionProps> = ({
       <div className="text-center flex justify-center mt-2 mb-6">
         <Icon className="text-5xl bg-transparent  text-center text-purple-700" />
       </div>
-      <h3 className="text-2xl font-semibold mb-2 text-[#843ab1]">{title}</h3>
-      <p className="text-gray-600 font-extralight">{description}</p>
+      <h3 className="sm:text-2xl text-xl font-semibold mb-2 text-[#843ab1]">{title}</h3>
+      <p className="text-gray-600 font-extralight sm:text-base text-sm">{description}</p>
       {/* <a
         href={buttonLink}
         className="inline-block px-4 py-1 bg-transparent text-purple-600 font-semibold mt-4 hover:text-white border-purple-600 border-2 rounded-md hover:bg-purple-700 transition"
