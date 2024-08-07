13a1f2c824543801070730b76e54f089fb470207
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-07 19:55:50 +0530

    refactor: Update mobile navigation styling and layout

--- src/app/cse-notes/layout.tsx
+++ src/app/cse-notes/layout.tsx
@@ -35,12 +35,13 @@ const CseNotesLayout = ({ children }: { children: React.ReactNode }) => {
   };
 
   const getNavItemClass = (path: string) => {
-    return pathname.startsWith(path) ? 'border-l-4 border-[#d998ff]' : 'hover:border-l-4 border-[#d998ff]';
+    return pathname.startsWith(path) ? 'border-l-2 border-[#d998ff]' : 'hover:border-l-2 border-[#d998ff]';
   };
 
   return (
-    <div className={denkOne.className}>
-      <div className="flex justify-between h-screen mt-6 rounded-xl">
+  <div>
+      <div className={denkOne.className}>
+      <div className="sm:flex hidden  justify-between h-screen mt-6 rounded-xl">
         <aside className={`bg-sidebar text-white rounded-xl transition-all duration-300 ${isOpen ? 'w-56' : 'w-20'}`}>
           <div className="flex flex-col justify-between">
             <div>
@@ -102,6 +103,74 @@ const CseNotesLayout = ({ children }: { children: React.ReactNode }) => {
 
       {isModalOpen && modalContent}
     </div>
+
+
+    {/* For mobile device  */}
+    <div className={denkOne.className}>
+      <div className="flex justify-between  h-screen mt-6 rounded-xl">
+        <aside className='bg-sidebar text-white rounded-xl transition-all duration-300 px-2'>
+          <div className="flex flex-col justify-center">
+            <div>
+              {/* <div className="flex cursor-pointer text-center items-center justify-between mt-10">
+                <button title="toggle" onClick={toggleSidebar} className="focus:outline-none">
+                  <FaBars size={16} />
+                </button>
+              </div> */}
+              <nav className="mt-10 flex-col justify-center">
+                <div
+                  className={`flex cursor-pointer my-4 py-0.5 items-center ${getNavItemClass('/cse-notes/1st-year')}`}
+                  onClick={() => openModal(<FirstYearModal isOpen={true} onClose={closeModal} title="First Year" yearPath="/cse-notes/1st-year" />)}
+                >
+                  <MdLooksOne size={20} />
+                  {isOpen && <span className="text-xl">1st Year</span>}
+                </div>
+                <div
+                  className={`flex cursor-pointer my-4 py-0.5 items-center ${getNavItemClass('/cse-notes/2nd-year')}`}
+                  onClick={() => openModal(<SecondYearModal isOpen={true} onClose={closeModal} title="Second Year" yearPath="/cse-notes/2nd-year" />)}
+                >
+                  <MdLooksTwo size={20} />
+                  {isOpen && <span className="text-xl">2nd Year</span>}
+                </div>
+                <div
+                  className={`flex cursor-pointer my-4 py-0.5 items-center space-x-1 ${getNavItemClass('/cse-notes/3rd-year')}`}
+                  onClick={() => openModal(<ThirdYearModal isOpen={true} onClose={closeModal} title="Third Year" yearPath="/cse-notes/3rd-year" />)}
+                >
+                  <MdLooks3 size={20} />
+                  {isOpen && <span className="text-xl">3rd Year</span>}
+                </div>
+                {/* <div
+                  className={`flex pl-6 cursor-pointer my-8 py-1 items-center space-x-2 ${getNavItemClass('/cse-notes/4th-year')}`}
+                  onClick={() => openModal(<FourthYearModal isOpen={true} onClose={closeModal} title="Fourth Year" yearPath="/cse-notes/4th-year" />)}
+                >
+                  <MdLooks4 size={32} />
+                  {isOpen && <span className="text-xl">4th Year</span>}
+                </div> */}
+                <div
+                  className={`flex cursor-pointer my-4 py-0.5 items-center ${getNavItemClass('/cse-notes/additional-notes')}`}
+                  onClick={() => router.push('/cse-notes/additional-notes')}
+                >
+                  <FiEdit size={20} />
+                  {isOpen && <span className="text-xl">Additional Notes</span>}
+                </div>
+              </nav>
+            </div>
+            <div className="cursor-pointer flex hover:border-l-2 border-white items-center bottom-0">
+              <Link href="https://google.com" target="_blank">
+                <button title="feedback" className="focus:outline-none flex">
+                  <FaRegComment size={20} />
+                  {isOpen && <span className="text-xl">FeedBack</span>}
+                </button>
+              </Link>
+            </div>
+          </div>
+        </aside>
+        <main className="">{children}</main>
+      </div>
+
+      {isModalOpen && modalContent}
+    </div>
+  </div>
+
   );
 };
 
