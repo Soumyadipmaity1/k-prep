5cf15dedbd3a7dc678a6ab58cc6cf23449796d06
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-08 01:44:53 +0530

    feat: Add new page component for CSE Labs

---
+++ src/app/cse-labs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-5xl flex justify-center items-center h-screen font-extrabold'>
+      Will Be Updated Soon !
+    </div>
+  )
+}
+
+export default page
