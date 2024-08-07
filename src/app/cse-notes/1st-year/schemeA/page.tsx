944e4e79102d13019511c4cc85c0a64e378f01fc
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-08 01:30:38 +0530

    refactor: Update CSE notes page layout and styling

--- src/app/cse-notes/1st-year/schemeA/page.tsx
+++ src/app/cse-notes/1st-year/schemeA/page.tsx
@@ -2,9 +2,9 @@ import SchemeASubject from "@/components/Subjects/1st-year/SchemeASub";
 
 const SchemeA = () => {
     return (
-        <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
-          <div className="max-h-screen font-bold p-10">
-<h1 className=" text-center text-4xl"><span className="modal-text">Pick a subject for your notes!</span></h1>
+        <div className="w-80 sm:w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl recommended-scrollbar border-gray-300">
+          <div className="max-h-screen font-bold sm:p-10 p-5">
+<h1 className=" text-center sm:text-4xl text-3xl"><span className="modal-text">Pick a subject for your notes!</span></h1>
 <div className="mt-10">
 
   <SchemeASubject/>
