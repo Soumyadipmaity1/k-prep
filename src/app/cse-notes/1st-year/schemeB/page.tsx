7c57a552bdac46f493aa2f6e6613504cecf21d66
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-11 15:16:59 +0530

    refactor: Update SchemeB page to use SchemeBSubject component

--- src/app/cse-notes/1st-year/schemeB/page.tsx
+++ src/app/cse-notes/1st-year/schemeB/page.tsx
@@ -2,7 +2,7 @@ import SchemeBSubject from "@/components/Subjects/1st-year/SchemeBSubject";
 
 const SchemeB= () => {
     return (
-        <div className=" w-full sm:w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl recommended-scrollbar border-gray-300">
+        <div className=" w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl recommended-scrollbar border-gray-300">
           <div className="max-h-screen font-bold sm:p-10 p-5">
 <h1 className=" text-center sm:text-4xl text-3xl"><span className="modal-text">Pick a subject for your notes!</span></h1>
 <div className="mt-10">
