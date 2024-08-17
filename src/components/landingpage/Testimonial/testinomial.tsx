db4124d41e225cfa9d17387200abcc34a657156a
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-17 11:39:42 +0530

    refactor: Update landing page Testimonial section content and styling

--- src/components/landingpage/Testimonial/testinomial.tsx
+++ src/components/landingpage/Testimonial/testinomial.tsx
@@ -29,9 +29,10 @@ const Testimonials: React.FC = () => {
     <section className="bg-white py-10 pb-16 sm:px-16">
       <div className="container mx-auto text-center">
         <h2 className="sm:text-5xl text-[38px] font-bold mb-6">What our users say about us?</h2>
-        <p className="text-gray-600 mb-12 px-4 sm:px-16">
-          Lorem ipsum dolor sit amet. Hic recusandae similique non culpa nulla ut repellendus sunt et voluptatem perferendis ex quae quisquam.
-          Qui nostrum voluptas qui repudiandae consequatur ut aliquid voluptas qui voluptas rerum.
+        <p className="text-gray-600 mb-12 px-4 font-sans font-semibold text-lg sm:px-16">
+        Has your benchmates been making better notes lately?!! They might be using K-prep. We assist trusted 500+ users with a database of 200+ notes and more.
+        Let&rsquo;s hear them and find out what our valued users think about us!!  
+        Feel free to add your comment and join our K-prep family!
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-1 sm:px-16">
           {testimonials.map((testimonial, index) => (
