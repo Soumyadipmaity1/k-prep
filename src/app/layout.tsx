192b1923ff4c01bd86c8306252359117f5742ae1
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-17 11:54:47 +0530

    refactor: Update body class in layout.tsx to include 'h-screen-minus-30' for full-screen height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={denkOne.className}>
-      <body className="h-screen sm:h-auto sm:p-5 body-shadow sm:m-5 m-2 p-3 rounded-md sm:rounded-xl">
+      <body className="h-screen-minus-30 sm:h-auto sm:p-5 body-shadow sm:m-5 m-2 p-3 rounded-md sm:rounded-xl">
 <div className=''>
 <Navbar />
 
