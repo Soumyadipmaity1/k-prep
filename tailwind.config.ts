d7e150e3ac602dc58f1cfbcc6087e61f58a06cb4
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-17 11:58:54 +0530

    refactor: Update 'screen-minus-30' height utility class in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
       },
       height: {
         'screen-minus-20': 'calc(100vh - 225px)',
-        'screen-minus-30': 'calc(100vh - 120px)',
+        'screen-minus-30': 'calc(100vh - 160px)',
 
       },
     },
