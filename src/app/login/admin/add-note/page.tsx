2d9cd4f8212ff2c9f4253c25ce1c45cc9bbfee28
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-25 15:21:10 +0530

    feat: Add AddNote page component

---
+++ src/app/login/admin/add-note/page.tsx
@@ -0,0 +1,12 @@
+import AddNoteForm from '@/components/AddNoteForm/AddnoteForm'
+import React from 'react'
+
+function AddNote() {
+  return (
+    <div>
+        <AddNoteForm/>
+    </div>
+  )
+}
+
+export default AddNote
