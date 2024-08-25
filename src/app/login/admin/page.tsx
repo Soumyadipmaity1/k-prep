c737c04e10386186087079914a9459d7289bc4c8
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-25 15:21:04 +0530

    feat: Add AdminDashboard component

---
+++ src/app/login/admin/page.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from '@/components/Menubar/AdminSidebar/AdminSidebar'
+import React from 'react'
+
+function AdminDashboard() {
+  return (
+    <div>
+      <AdminSidebar/>
+    </div>
+  )
+}
+
+export default AdminDashboard
