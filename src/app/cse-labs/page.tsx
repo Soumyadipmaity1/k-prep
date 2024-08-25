ca0d687ad326c214d688f719eef0f9a27b80f876
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-25 15:20:37 +0530

    refactor: Add Navbar component to CseNotesLayout

--- src/app/cse-labs/page.tsx
+++ src/app/cse-labs/page.tsx
@@ -1,10 +1,14 @@
+import { Navbar } from '@nextui-org/react'
 import React from 'react'
 
 function page() {
   return (
-    <div className='text-5xl flex justify-center items-center h-screen font-extrabold'>
+   <div>
+    <Navbar/>
+     <div className='text-5xl flex justify-center items-center h-screen font-extrabold'>
       Will Be Updated Soon !
     </div>
+   </div>
   )
 }
 
