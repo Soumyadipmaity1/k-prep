61833870d2999d1292d0d48e0d4955691768070c
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-25 15:20:25 +0530

    feat: Add Navbar component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import { denkOne } from './font';
 import dynamic from 'next/dynamic';
 import Hero from '@/components/landingpage/Hero/Hero';
 import Footer from '@/components/Footer/footer';
+import Navbar from '@/components/Menubar/Navbar/Navbar';
 
 export default function Home() {
   return (
     <div className={denkOne.className}>
+      <Navbar />
+
 <div className=''>
 <Hero/>
       {/* <MaterialSlider/> */}
