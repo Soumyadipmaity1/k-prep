a3381cf410e41efeb033e519d033b2b2a00ae5ba
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-15 12:43:02 +0530

    Refactor ViewNotes component: Update placeholder text

--- src/app/admin/view-note/page.tsx
+++ src/app/admin/view-note/page.tsx
@@ -3,7 +3,7 @@ const ViewNotes = () => {
   return (
     <div className="p-6 mx-auto max-w-3xl  ">
       <h2 className="text-xl font-semibold mb-4">View Notes</h2>
-      <p className="text-gray-700">This page is not yet implemented.</p>
+      <p className="text-gray-700">This page is for admin to view all the notes.</p>
     </div>
   );
 };
