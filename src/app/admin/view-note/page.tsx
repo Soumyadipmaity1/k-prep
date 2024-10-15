3d34c5ad87870ed63ad0cff3ba38666ea22498f7
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-15 12:35:59 +0530

    Add ViewNotes component

---
+++ src/app/admin/view-note/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+const ViewNotes = () => {
+  return (
+    <div className="p-6 mx-auto max-w-3xl  ">
+      <h2 className="text-xl font-semibold mb-4">View Notes</h2>
+      <p className="text-gray-700">This page is not yet implemented.</p>
+    </div>
+  );
+};
+
+export default ViewNotes;
