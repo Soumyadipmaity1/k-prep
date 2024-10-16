22a8f2ab6f59b8737138d3e6ba15435770a5cdc1
Abhik Patra <theabhik2020@gmail.com>
2024-10-16 22:28:11 +0530

    add view note table

--- src/app/admin/view-note/page.tsx
+++ src/app/admin/view-note/page.tsx
@@ -1,9 +1,13 @@
 "use client";
+
+import ShowNotes from "@/components/showNotes/show-notes";
+
 const ViewNotes = () => {
   return (
     <div className="p-6 mx-auto max-w-3xl  ">
       <h2 className="text-xl font-semibold mb-4">View Notes</h2>
       <p className="text-gray-700">This page is for admin to view all the notes.</p>
+      <ShowNotes/>
     </div>
   );
 };
